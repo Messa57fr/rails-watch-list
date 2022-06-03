55f90fde61c122a26c81d41001100d97b8011874
Samuel Coizet <messa57fr@gmail.com>
2022-06-03 15:20:32 +0200

    finish backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
   resources :lists, only: %i[index create new show] do
-    resources :bookmarks, only: %i[create new destroy]
+    resources :bookmarks, only: %i[create new]
   end
+  resources :bookmarks, only: %i[destroy]
 end
